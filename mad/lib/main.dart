fb33990ca9686620eed8832307cffe00fd2ad01e
Azizkhan22 <azizkhan22kakar@gmail.com>
2025-04-24 20:59:57 +0500

    Create Lab 7 with all tasks

--- mad/lib/main.dart
+++ mad/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mad/lab%206/task3.dart';
+import 'package:mad/lab%207/task2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,26 +13,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Lab 3',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+      title: 'Lab 7',
+      theme: ThemeData(      
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
-      home: DashboardScreen()
+      home: PostView()
     );
   }
 }
